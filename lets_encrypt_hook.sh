92728c0dc7cf2b8af5155f8b9d3f4e68d83fee2e
Frederick Henderson <frederickjh@henderson-meier.org>
2020-02-17 16:21:58 +0100

    Added color to the error hooks.

--- lets_encrypt_hook.sh
+++ lets_encrypt_hook.sh
@@ -10,6 +10,20 @@ challengetoken=$3
 dnstoken=$4
 
 
+function loadcolor(){
+# Colors  http://wiki.bash-hackers.org/snipplets/add_color_to_your_scripts
+# More info about colors in bash http://misc.flogisoft.com/bash/tip_colors_and_formatting
+esc_seq="\x1b["  #In Bash, the <Esc> character can be obtained with the following syntaxes:  \e  \033  \x1B
+NC=$esc_seq"39;49;00m" # NC = Normal Color
+# Colors with black background (40;)set for emails.
+red=$esc_seq"31;40;01m"
+green=$esc_seq"32;40;00m"
+yellow=$esc_seq"33;40;01m"
+blue=$esc_seq"34;40;01m"
+magenta=$esc_seq"35;40;01m"
+cyan=$esc_seq"36;40;01m"
+}
+loadcolor
 #Possible operations are: clean_challenge, deploy_challenge, deploy_cert, invalid_challenge or request_failure
 case $operation in
 deploy_challenge)
@@ -27,10 +41,10 @@ deploy_cert)
   dnsexit-deploy-hook.sh
   ;;
 invalid_challenge)
-    echo "The challenge was invalid."
+    echo -e "${yellow}>>${red}The challenge was invalid.${NC}"
   ;;
 request_failure)
-    echo "The request failed."
+    echo -e "${yellow}>>${red}The request failed.${NC}"
   ;;
 *)
   echo ""
